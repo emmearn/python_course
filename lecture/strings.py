f16e40485e6577ae26faa06c035fd79bfd503f49
Marco <marcoarnone@easy-network.it>
2019-05-28 16:40:55 +0200

    string

---
+++ lecture/strings.py
@@ -0,0 +1,14 @@
+s = ''
+
+s = ""
+
+s = 'This is a ' \
+    'multi line string'
+
+s = """This is also a 
+    multi line string"""
+
+s = '''This is another king of
+    multi line string'''
+
+s = 'vado \na capo'  # \t for tab
