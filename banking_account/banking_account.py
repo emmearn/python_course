b833a157ddb9972779ad88a128a2a72d3931b377
Marco <marcoarnone@easy-network.it>
2019-05-31 16:10:46 +0200

    banking account exercise

---
+++ banking_account/banking_account.py
@@ -0,0 +1,46 @@
+class BankingAccount:
+    def __init__(self, name, account, amount):
+        self.name = name
+        self.account = account
+        self.__amount = amount
+
+    @property
+    def amount(self):
+        print("I'm calling the getter method")
+        return self.__amount
+
+    @amount.setter
+    def amount(self, amount):
+        print("I'm calling the setter method")
+        self.__amount = amount
+
+    def withdraw(self, amount):
+        self.__amount -= amount
+
+    def deposit(self, amount):
+        self.__amount += amount
+
+    def to_string(self):
+        print(f'{self.name} {self.account} ${self.amount}')
+
+
+banking_account1 = BankingAccount('Mario Rossi', 123, 0)
+banking_account2 = BankingAccount('Mario Bianchi', 124, 0)
+
+banking_account1.amount = 10000  # I'm calling the setter method
+banking_account2.amount = 6000  # I'm calling the setter method
+
+banking_account1.to_string()  # I'm calling the getter method  # Mario Rossi 123 $10000
+banking_account2.to_string()  # I'm calling the getter method  # Mario Bianchi 124 $6000
+
+for count in range(1, 4):
+    banking_account1.withdraw(1000)
+
+banking_account1.deposit(100)
+
+banking_account1.to_string()  # I'm calling the getter method  # Mario Rossi 123 $7100
+
+for count in range(1, 100):
+    banking_account2.withdraw(10 + count)
+
+banking_account2.to_string()  # I'm calling the getter method  # Mario Bianchi 124 $60
